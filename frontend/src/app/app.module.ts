72e292f64ac29788fdf902f74fa5f70612b83370
Denis Macedo <denisrmacedo@gmail.com>
2023-02-21 00:22:24 -0300

    create router

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './components/views/home/home.component';
+import { ProductCrudComponent } from './components/views/product-crud/product-crud.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { HomeComponent } from './components/views/home/home.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
